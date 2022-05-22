e2eca51b5e3a1d0d8ca748193145790ca3726068
Lopez <ivan-rojas@outlook.com>
2022-05-21 23:44:12 -0300

    💟 - API REST for Products completed

--- app/api/products_api.py
+++ app/api/products_api.py
@@ -1,9 +1,57 @@
-from __main__ import app
-from flask import jsonify
-from models.product import Product, products_schema
+from __main__ import app, request
+from db.database import db
+from models.product import Product, products_schema, product_schema
 
-@app.route('/')
-def index():
+ROUTE = '/product'
+ROUTE_ID = '/product/<id>'
+
+# Get All
+@app.route(ROUTE, methods=['GET'])
+def get_all():
     product_list = Product.query.all()
-    result = products_schema.dump(product_list)
-    return jsonify(result)
+    return products_schema.jsonify(product_list)
+
+# Get
+@app.route(ROUTE_ID, methods=['GET'])
+def get(id):
+    product = Product.query.get(id)
+    return product_schema.jsonify(product)
+
+# Add
+@app.route(ROUTE, methods=['POST'])
+def add():
+    name = request.json['name']
+    amount = request.json['amount']
+    image = request.json['image']
+    active = request.json['active']
+
+    product = Product(name, amount, image, active)
+    db.session.add(product)
+    db.session.commit()
+    return product_schema.jsonify(product)
+
+# Update
+@app.route(ROUTE_ID, methods=['PUT'])
+def update(id):
+    product = Product.query.get(id)
+
+    name = request.json['name']
+    amount = request.json['amount']
+    image = request.json['image']
+    active = request.json['active']
+
+    product.name = name
+    product.amount = amount
+    product.image = image
+    product.active = active
+
+    db.session.commit()
+    return product_schema.jsonify(product)
+
+# Delete
+@app.route(ROUTE_ID, methods=['DELETE'])
+def delete(id):
+    product = Product.query.get(id)
+    db.session.delete(product)
+    db.session.commit()
+    return product_schema.jsonify(product)
