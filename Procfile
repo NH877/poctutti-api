acecb4c3c22b7ab11e91dd9fdfc1b943ebc4349f
Lopez <ivan-rojas@outlook.com>
2022-11-13 16:01:26 -0300

    #️⃣ - Heroku deploy configurations

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi:app
+web: gunicorn --chdir app app:app
--- app/config.py
+++ app/config.py
@@ -1,3 +1,3 @@
 class BaseConfig:
-    DATABASE_URI = 'postgresql://aqvwfwcpystbgx:b601a0d01f4cb762589fddc802021c578e46308fb822cd5d6523f785a806fef7@ec2-34-236-94-53.compute-1.amazonaws.com:5432/d7dm2us6j1gssm'
+    DATABASE_URI = 'postgresql://rflvmirrwvqdwl:7a4ada77323659ef72fcf46ce1aaa4b67d70cb429e903d4aa9625df463a1f224@ec2-52-1-17-228.compute-1.amazonaws.com:5432/dp0g07cnlekj3'
     DATABASE_TRACK_MODIFICATIONS = False
