925a7784b92e3b22ed8a2a98cbf3c5ccf6526b25
Lopez <ivan-rojas@outlook.com>
2022-11-14 18:25:55 -0300

    ⛅ - CORS implemented

--- app/app.py
+++ app/app.py
@@ -1,13 +1,17 @@
-from flask import Flask, request
+from flask import Flask
 from db.database import db, ma
 from config import BaseConfig
+from flask_cors import CORS
 
 # Init application
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = BaseConfig.DATABASE_URI
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = BaseConfig.DATABASE_TRACK_MODIFICATIONS
+
+# Init CORS
+CORS(app, origins=['*'])
 
 # Init database
+app.config['SQLALCHEMY_DATABASE_URI'] = BaseConfig.DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = BaseConfig.DATABASE_TRACK_MODIFICATIONS
 db.init_app(app)
 ma.init_app(app)
 
@@ -15,4 +19,3 @@ ma.init_app(app)
 import api.products_api
 import api.stores_api
 import api.sales_api
-
